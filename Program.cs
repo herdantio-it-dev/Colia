58548fbc49514d380b8d6373fdb389ce4a4d3a69
EljabloElmore <jovenn176@gmail.com>
2026-07-09 09:13:26 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using Colia;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+using static Colia.Unit;
+
+Characters tb = new Characters("Trailblazer", 980, 20, 8);
+Characters dh = new Characters("DanHeng", 714, 25, 3);
+Characters m7 = new Characters("March7th", 857, 14, 6, true, 180);
+Boss cocolia = new Boss("Cocolia", 1307, 42, 10);
+Random random = new Random();
+
+tb.MaxHp = 980;
+dh.MaxHp = 714;
+m7.MaxHp = 857;
+cocolia.MaxHp = 1307;
+
+List<Unit> team = new List<Unit> { tb, dh, m7 };
+List<Unit> boss = new List<Unit> { cocolia };
+
+Console.WriteLine("--------------Battle Start--------------");
+while (!cocolia.isDead && !(tb.isDead && dh.isDead && m7.isDead))
+{
+    Console.WriteLine($"{cocolia.Name}      HP: {cocolia.Hp}/{cocolia.MaxHp}");
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine($"{tb.Name}  HP: {tb.Hp}/{tb.MaxHp}");
+    Console.WriteLine($"{dh.Name}      Hp: {dh.Hp}/{dh.MaxHp}");
+    Console.WriteLine($"{m7.Name}     Hp: {m7.Hp}/{m7.MaxHp}");
+    Console.WriteLine("");
+    Console.WriteLine("");
+
+
+    if (!cocolia.isDead)
+    {
+        Console.WriteLine("Cocolia Turn!");
+        Console.WriteLine("----------------------------------------");
+        int attack = random.Next(0, 2);
+        var alive = team.FindAll(mc => !mc.isDead);
+
+        if (attack == 0)
+        {
+            Unit target = alive[random.Next(alive.Count)];
+            cocolia.Attack(target);
+            Console.WriteLine("----------------------------------------");
+        }
+        else
+        {
+            cocolia.UseUltimate(team, boss);
+            Console.WriteLine("----------------------------------------");
+        }
+        GameStatus(tb, dh, m7, cocolia);
+
+    }
+
+    if (!tb.isDead && !cocolia.isDead)
+    {
+        PlayerTurn(tb, team, boss);
+        GameStatus(tb, dh, m7, cocolia);
+    }
+
+    if (!dh.isDead && !cocolia.isDead)
+    {
+        PlayerTurn(dh, team, boss);
+        GameStatus(tb, dh, m7, cocolia);
+    }
+
+    if (!m7.isDead && !cocolia.isDead)
+    {
+        PlayerTurn(m7, team, boss);
+        GameStatus(tb, dh, m7, cocolia);
+    }
+
+    static void PlayerTurn(Characters character, List<Unit> team, List<Unit> boss)
+    {
+        Console.WriteLine($"{character.Name} Turn");
+        Console.WriteLine("1.Basic Attack");
+        Console.WriteLine("2.Ultimate");
+        Console.WriteLine("----------------------------------------");
+        string option = Console.ReadLine();
+
+        if (option == "1")
+        {
+            character.Attack(boss[0]);
+        }
+        else if (option == "2")
+        {
+            character.UseUltimate(team, boss);
+        }
+    }
+
+    static void GameStatus(Characters tb, Characters dh, Characters m7, Boss cocolia)
+    {
+        if (cocolia.isDead)
+        {
+            Console.WriteLine("VICTORY");
+        }
+
+        if (tb.isDead && dh.isDead && m7.isDead)
+        {
+            Console.WriteLine("DEFEATED");
+        }
+    }
+
+}
---
+++ Unit.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Colia
+{
+
+    public interface IComabat
+    {
+        void Damage(int TrueDamage);
+        void Attack(Unit target);
+        void UseUltimate(List<Unit> team, List<Unit> boss);
+    }
+
+    public class Unit : IComabat
+    {
+        public string Name { get; set; }
+        public int Hp { get; set; }
+        public int Atk { get; set; }
+        public int Def { get; set; }
+        public int MaxHp { get; set; }
+        public bool isDead { get; set; }
+
+        public Random random = new Random();
+
+        public Unit(string name, int hp, int atk, int def)
+        {
+            Name = name;
+            MaxHp = hp;
+            Hp = hp;
+            Atk = atk;
+            Def = def;
+        }
+
+        public void Damage(int TrueDamage)
+        {
+            int DamageTotal = TrueDamage - Def;
+            Hp = Math.Max(0, Hp - DamageTotal);
+
+            Console.WriteLine($"{Name} has taken {DamageTotal} amount of damage, HP: {Hp}/{MaxHp}");
+            Console.WriteLine("----------------------------------------");
+
+            if (Hp == 0)
+            {
+                isDead = true;
+                Console.WriteLine($"{Name} is down");
+                Console.WriteLine("----------------------------------------");
+            }
+        }
+
+        public virtual void Attack(Unit target)
+        {
+            Console.WriteLine($"{Name} attacks {target.Name}");
+            Console.WriteLine("----------------------------------------");
+            target.Damage(Atk);
+        }
+
+        public virtual void UseUltimate(List<Unit> team, List<Unit> boss)
+        {
+
+        }
+
+        public class Characters : Unit
+        {
+            public bool isHealer;
+            public int Heal;
+            public Characters(string name, int hp, int atk, int def, bool isHealer = false, int Heal = 0)
+            : base(name, hp, atk, def)
+            {
+                this.isHealer = isHealer;
+                this.Heal = Heal;
+            }
+
+            public override void UseUltimate(List<Unit> team, List<Unit> boss)
+            {
+                Console.WriteLine($"{Name} used an Ultimate");
+                Console.WriteLine("----------------------------------------");
+                if (isHealer)
+                {
+                    Console.WriteLine($"{Name} heals everyone");
+                    foreach (var character in team)
+                    {
+                        if (!character.isDead)
+                        {
+                            character.Hp = Math.Min(character.MaxHp, character.Hp + Heal);
+                            Console.WriteLine($"{character.Name} Healed the team by {Heal}, HP: {character.Hp}/{character.MaxHp}");
+                        }
+
+                        else
+                        {
+                            int rng = random.Next(2, 11);
+                            int DamageTotal = Atk + rng;
+                            boss[0].Damage(DamageTotal);
+                            //Console.WriteLine($"{Name} dealt {rngDamage} amount of damage");
+                        }
+                    }
+                }
+            }
+        }
+
+        public class Boss : Unit
+        {
+            public Boss(string Name, int Hp, int Atk, int Def) : base(Name, Hp, Atk, Def)
+            {
+
+            }
+
+            public override void UseUltimate(List<Unit> team, List<Unit> boss)
+            {
+                Console.WriteLine($"Freeze to death, {Name} used Blizzard");
+                Console.WriteLine("----------------------------------------");
+
+                var alive = team.FindAll(mc => !mc.isDead);
+
+                foreach (var target in alive)
+                {
+                    var rng = random.Next(2, 11);
+                    var rngDamage = Atk + rng;
+                    target.Damage(rngDamage);
+                }
+            }
+        }
+
+    }
+
+}
